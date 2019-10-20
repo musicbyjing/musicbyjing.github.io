44b9b82e529be5f4403d816507017d029274ca8b
musicbyjing <musicbyjing@gmail.com>
2019-10-20 15:47:09 -0400

    Change background colors and text color

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Welcome from "./components/Welcome";
 import MyGallery from "./components/MyGallery";
 
 const colors = [
-  "#212822",
-  "#d3d9ec",
-  "#a66473",
-  "#75526e",
-  "#545676",
-  "#878590",
-  "#c5b2d6"
+  "#75526e", // dark purple
+  "#545676", // dark blue
+  "#a66473", // medium red
+  "#878590", // gray
+  "#c5b2d6" // light purple
+  // Text colors:
+  // "#212822", // dark green
+  // "#d3d9ec", // sky blue
 ];
+
+const textColor = bgColor => {
+  if (bgColor === "#75526e" || bgColor === "#545676") return "#d3d9ec";
+  else return "#212822";
+};
 
 export default class App extends Component {
   constructor(props) {
@@ -30,7 +36,7 @@ export default class App extends Component {
     let bgColor = colors[Math.floor(Math.random() * colors.length)];
     const { gallery } = this.state;
     return (
-      <div style={{ backgroundColor: bgColor }}>
+      <div style={{ backgroundColor: bgColor, color: textColor(bgColor) }}>
         {gallery === "back" && <Welcome handleSubmit={this.handleSubmit} />}
         {gallery !== "back" && (
           <MyGallery
